ab9e8dc29ee31e3323da881b491aa400536aad5e
Iko Assa <Iko@ampa.co.il>
2019-06-11 14:46:43 +0300

    my first comment

---
+++ script.js
@@ -0,0 +1,25 @@
+const maxCapacity = 14
+let surfTime = true
+let bestStudent = 'sagi'
+const purposeInLife = 1
+
+console.log(maxCapacity)
+console.log(surfTime)
+console.log(bestStudent)
+console.log(purposeInLife)
+
+let a= 'nba';
+a+= ' is the best thing ever!'
+console.log(a)
+
+let password = 'damn'
+let confirmpassword = 'god damn'
+console.log(password==confirmpassword)
+
+
+console.log(423*12)
+console.log(802/2)
+console.log((6+5)*3)
+console.log(undefined || null)
+
+
